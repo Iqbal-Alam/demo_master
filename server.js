dec9020fab5984fdd531920d152b66a559b26188
Sameer Rehan <Sameer Rehan>
2018-07-31 01:17:18 +0530

    deployed automatic to heroku

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/index.html'));
+// });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+var express = require('express'),
+    app = express();
+
+app.use(express.static(__dirname + '/dist'));
+app.get('/', function (req, res, next) {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers['x-forwarded-proto'] != 'https') {
+            return res.redirect('https://www.google.com');
+        } else {
+            return next();
+        }
+    } else {
+        return next();
+    }
+})
+var server = app.listen(process.env.PORT || 8080);
