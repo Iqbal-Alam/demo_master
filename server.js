e7a5caa5786f42dde972ba8ad8ca3a7c822fd0c3
Iqbal-Alam <iqbalalam60@gmail.com>
2018-07-31 10:40:30 +0530

    updated server file.

--- server.js
+++ server.js
@@ -1,33 +1,9 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist'));
-
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname+'/dist/index.html'));
-// });
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
-var express = require('express'),
-    app = express();
-
+// server.js
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.get('/', function (req, res, next) {
-    if (process.env.NODE_ENV === 'production') {
-        if (req.headers['x-forwarded-proto'] != 'https') {
-            return res.redirect('https://www.google.com');
-        } else {
-            return next();
-        }
-    } else {
-        return next();
-    }
-})
-var server = app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
